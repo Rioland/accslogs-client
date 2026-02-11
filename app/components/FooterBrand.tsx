e605e99ee7804fe5ff4855a8c71258b72b3e9745
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2026-02-11 07:37:55 -0500

    update

--- app/components/FooterBrand.tsx
+++ app/components/FooterBrand.tsx
@@ -10,9 +10,7 @@ export default function FooterBrand() {
           New Ticket / Ask a Question
         </button>
 
-        <button className="bg-orange-500 text-white px-4 py-2 rounded-md text-sm">
-          Become a Supplier
-        </button>
+       
       </div>
     </div>
   );
