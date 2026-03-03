2c29731c865ca784431c9a901d697eaae5974c28
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2026-03-03 03:45:40 -0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,24 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         {children}
         <Toaster />
+        {/* Tawk.to Live Chat */}
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/699ef4e2b2f3e31c2e620e99/1jiaep7h1';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
