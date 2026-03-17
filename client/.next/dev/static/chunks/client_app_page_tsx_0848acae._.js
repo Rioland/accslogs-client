49aca0fd055e164940011a00bce47916ee8b13ef
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2026-03-17 03:54:44 -0400

    update

--- client/.next/dev/static/chunks/client_app_page_tsx_0848acae._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/client_app_components_Navbar2_tsx_230d2ba0._.js",
-  "static/chunks/client_fc809fa5._.js",
-  "static/chunks/1e749_52cce453._.js"
-],
-    source: "dynamic"
-});
